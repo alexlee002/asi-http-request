bab31d6310ac6154b60802d71cb4e3893a975faa
Jardel Weyrich <jweyrich@gmail.com>
2012-08-20 15:02:26 -0300

    Avoid warnings about implicit integer conversions that change the signedness of an integer value (-Wsign-conversion).

--- Classes/ASIInputStream.m
+++ Classes/ASIInputStream.m
@@ -144,7 +144,7 @@ static NSLock *readLock = nil;
 	[readLock unlock];
 	NSInteger rv = [stream read:buffer maxLength:toRead];
 	if (rv > 0)
-		[ASIHTTPRequest incrementBandwidthUsedInLastSecond:rv];
+		[ASIHTTPRequest incrementBandwidthUsedInLastSecond:(NSUInteger)rv];
 	return rv;
 }
 
